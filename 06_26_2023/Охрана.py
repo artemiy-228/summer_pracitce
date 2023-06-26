ea2798d5cdd33e6ae6473078362bcccf464487f8
artemiy-228 <artemsch17@gmail.com>
2023-06-26 19:28:58 +0500

    Not ready

---
+++ 06_26_2023/Охрана.py
@@ -0,0 +1,34 @@
+n = int(input())
+answers=[]
+
+def calculate(s, cords):
+    for j in range(1, s):
+        for k in range(1, s):
+            max = -1
+            d = 0
+            for c in cords:
+                if ((c[0] - k) ** 2 + (c[1] - j) ** 2) ** 0.5 > max:
+                    max = ((c[0] - k) ** 2 + (c[1] - j) ** 2) ** 0.5
+
+            if s - j > max and j > max and s - k > max and k > max:
+                return f"dog {k} {j}"
+    return "robot"
+
+
+for i in range(n):
+    s, h = map(int, input().split())
+    cords = []
+
+    for j in range(h):
+        cords.append(tuple(map(int, input().split())))
+
+    answers.append(calculate(s, cords))
+
+for i in answers:
+    print(i)
+
+
+
+
+
+
